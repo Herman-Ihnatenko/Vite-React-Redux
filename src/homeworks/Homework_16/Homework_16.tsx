843f782dbd5a8078d2dd05fbb94b920bf4e7f817
Herman-Ihnatenko <german1514u@gmail.com>
2025-09-19 19:34:21 +0200

    homework_17

---
+++ src/homeworks/Homework_16/Homework_16.tsx
@@ -0,0 +1,11 @@
+import Feedback from "components/Feedback/Feedback"
+import { PageWrapper } from "./styles"
+const Homework_16 = () => {
+  return (
+    <PageWrapper>
+      <Feedback />
+    </PageWrapper>
+  )
+}
+
+export default Homework_16
