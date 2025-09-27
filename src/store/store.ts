693c389d0fc3bee821ace22ec2e1f66baa228f9b
Herman-Ihnatenko <german1514u@gmail.com>
2025-09-27 13:49:00 +0200

    Lesson

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
+
 import { counterSlice } from "store/redux/counter/counterSlice"
+import { feedbackSlice } from "store/redux/feedback/feedbackSlice"
 import { catFactSlice } from "store/redux/CatFact/catFactSlice"
+import { randomJokeSlice } from "store/redux/randomJoke/randomJokeSlice"
+
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(counterSlice, catFactSlice)
+const rootReducer = combineSlices(counterSlice, feedbackSlice, catFactSlice, randomJokeSlice)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
@@ -13,15 +17,9 @@ export type RootState = ReturnType<typeof rootReducer>
 export const makeStore = (preloadedState?: Partial<RootState>) => {
   const store = configureStore({
     reducer: rootReducer,
-    // Adding the api middleware enables caching, invalidation, polling,
-    // and other useful features of `rtk-query`.
-    middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware()
-    },
     preloadedState,
   })
-  // configure listeners using the provided defaults
-  // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+
   return store
 }
 
